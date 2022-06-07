e6c3b5a83a990df2a3ed3be534178a266a257b93
rio <75190035+HyoJongPark@users.noreply.github.com>
2022-06-07 15:44:52 +0900

    스터디 개설 알림 구현
    - 인터셉터를 사용해 모든 요청에 웹 알림이 적용되도록 구현
    - 알림 아이콘 뷰 수정

--- src/main/java/me/soodo/studyolle/infra/main/MainController.java
+++ src/main/java/me/soodo/studyolle/infra/main/MainController.java
@@ -1,19 +1,27 @@
 package me.soodo.studyolle.infra.main;
 
+import lombok.RequiredArgsConstructor;
 import me.soodo.studyolle.modules.account.CurrentUser;
 import me.soodo.studyolle.modules.account.Account;
+import me.soodo.studyolle.modules.notification.NotificationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+
+    private final NotificationRepository notificationRepository;
 
     @GetMapping("/")
     public String home(@CurrentUser Account account, Model model) {
         if (account != null) {
             model.addAttribute(account);
         }
+
+        long count = notificationRepository.countByAccountAndChecked(account, false);
+        model.addAttribute("hasNotification", count > 0);
 
         return "index";
     }
